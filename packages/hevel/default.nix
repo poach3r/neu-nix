9b8a3eb98f25ea0ef9832be56d215dde24dc1de3
ricmaps <ricardomapurungajunior@gmail.com>
2026-03-08 18:58:07 -0300

    add hevel

---
+++ packages/hevel/default.nix
@@ -0,0 +1,56 @@
+{
+  stdenv,
+  fetchFromSourcehut,
+  bmake,
+  neuwld,
+  neuswc,
+  libinput,
+  pixman,
+  pkg-config,
+  fontconfig,
+  wayland,
+  wayland-scanner,
+  wayland-protocols,
+  libdrm,
+  libxkbcommon,
+  libxcb,
+  libxcb-wm,
+  udev,
+}:
+stdenv.mkDerivation {
+  pname = "hevel";
+  version = "0.0";
+  src = fetchFromSourcehut {
+    owner = "~dlm";
+    repo = "hevel";
+    rev = "bb9416a41ca7b33536a5f52a37752a854d68661c";
+    hash = "sha256-oThop0LaQQthkr+9rVX0kGi528IcP4l29z41AjxbNJM=";
+  };
+
+  nativeBuildInputs = [
+    bmake
+    pkg-config
+    wayland-scanner
+  ];
+
+  buildInputs = [
+    libdrm
+    libinput
+    pixman
+    neuswc
+    neuwld
+    wayland
+    wayland-protocols
+    libxkbcommon
+    libxcb
+    libxcb-wm
+    udev
+    fontconfig
+  ];
+
+  makeFlags = [
+    "PREFIX=$(out)"
+  ];
+
+  PKG_CONFIG_PATH = "${neuswc}/lib/pkgconfig:$PKG_CONFIG_PATH";
+}
